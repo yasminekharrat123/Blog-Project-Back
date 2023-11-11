3f02a4cbc43dcab217bce724b95815fd8fcab215
skandertebo <alexandertebourb@gmail.com>
2023-11-11 11:30:40 +0100

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blog.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public UserRole Role { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+
+        /*// Navigation properties
+        public Admin Admin { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<Like> Likes { get; set; }*/
+    }
+
+    public enum UserRole
+    {
+        SuperAdmin,
+        Admin,
+        User
+    }
+}
