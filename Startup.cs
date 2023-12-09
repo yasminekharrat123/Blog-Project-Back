9aab0c7c1c0e7c667ea7fe00e4a1e5490a7d3765
ykharrat <yasmine.kharrat@covectors.io>
2023-12-09 15:56:15 +0100

    added swagger documentation

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,23 @@
 using AI;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+
+
+
 
 public class Startup
 
 {
-     private readonly IConfiguration _configuration;
+    private readonly IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers(); 
+        services.AddControllers();
         services.AddRazorPages();
         services.AddHttpClient("OpenAI", client =>
         {
@@ -20,34 +26,52 @@ public class Startup
             string openAiApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? throw new Exception("OPENAI_API_KEY environment variable is not set.");
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {openAiApiKey}");
         });
-        
+
         services.AddTransient<IOpenAIService, OpenAIService>();
 
-        
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Blog website API",
+                Version = "v1",
+                Description = "An API for a blogging website lol"
+            });
+        });
+
+
 
     }
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-            // app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-            });
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        // app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+            endpoints.MapControllers();
+        });
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog website API");
+            c.RoutePrefix = string.Empty;
+        });
     }
+}
 
